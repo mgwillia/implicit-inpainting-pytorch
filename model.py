a3fe0824159465c186e1cd4f9ead510f23522358
Matt Gwilliam <mattgwilliamjr@gmail.com>
2022-03-01 19:47:09 -0500

    y

--- model.py
+++ model.py
@@ -2,13 +2,39 @@ import torch
 import torch.nn as nn
 
 
+class Conv2dSame(torch.nn.Conv2d):
+
+    def calc_same_pad(self, i: int, k: int, s: int, d: int) -> int:
+        return max((math.ceil(i / s) - 1) * s + (k - 1) * d + 1 - i, 0)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        ih, iw = x.size()[-2:]
+
+        pad_h = self.calc_same_pad(i=ih, k=self.kernel_size[0], s=self.stride[0], d=self.dilation[0])
+        pad_w = self.calc_same_pad(i=iw, k=self.kernel_size[1], s=self.stride[1], d=self.dilation[1])
+
+        if pad_h > 0 or pad_w > 0:
+            x = F.pad(
+                x, [pad_w // 2, pad_w - pad_w // 2, pad_h // 2, pad_h - pad_h // 2]
+            )
+        return F.conv2d(
+            x,
+            self.weight,
+            self.bias,
+            self.stride,
+            self.padding,
+            self.dilation,
+            self.groups,
+        )
+
+
 class GenConv(nn.Module):
     def __init__(self, in_channels, out_channels, ksize, stride=1, rate=1, activation=nn.ELU()):
         super(GenConv, self).__init__()
         self.activation = activation
         self.activation = activation
         self.sigmoid = nn.Sigmoid()
-        self.conv = nn.Conv2d(in_channels, out_channels, ksize, stride, dilation=rate, padding='same')
+        self.conv = nn.Conv2dSame(in_channels, out_channels, ksize, stride, dilation=rate)
 
 
     def forward(self, x):
